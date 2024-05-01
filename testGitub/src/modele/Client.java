c432f054492700dbb03cf2145609d51ffc4275ac
M.SY <80853826+khalidouafi@users.noreply.github.com>
2024-05-01 16:02:02 +0000

    Premier commit : ajout de mon projet Java

---
+++ testGitub/src/modele/Client.java
@@ -0,0 +1,31 @@
+package modele;
+
+public class Client {
+private String prenom, nom, login;
+
+public String getPrenom() {
+	return prenom;
+}
+
+public void setPrenom(String prenom) {
+	this.prenom = prenom;
+}
+
+public String getNom() {
+	return nom;
+}
+
+public void setNom(String nom) {
+	this.nom = nom;
+}
+
+public String getLogin() {
+	return login;
+}
+
+public void setLogin(String login) {
+	this.login = login;
+}
+
+
+}
